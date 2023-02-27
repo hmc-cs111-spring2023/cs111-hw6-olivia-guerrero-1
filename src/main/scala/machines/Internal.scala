aa5f95bfa541a037fc501195a3e1bedaa531203c
Olivia Guerrero <112516748+OliveBattle@users.noreply.github.com>
2023-02-27 00:02:48 +0000

    Part 2
    more weird errors this is terrifying

--- src/main/scala/machines/Internal.scala
+++ src/main/scala/machines/Internal.scala
@@ -3,4 +3,12 @@ package machines
 import regex._
 import dfa._
 
-given Conversion[Char, RegularLanguage] = c => Character(c)
+given Conversion[Char, RegularLanguage] = Character(_)
+
+given Conversion[String, RegularLanguage] = s =>
+    simplify( //Then simplify out the unnecessary complexity
+        s.foldRight(Concat(Epsilon, Epsilon)) //Scala requires foldright result as same type
+        ((c, rest) => Concat(Character(c), rest))
+    )
+
+//given Conversion[String, RegularLanguage] = val charList = _.map(Character)
